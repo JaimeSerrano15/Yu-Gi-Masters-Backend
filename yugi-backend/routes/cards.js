04693d8c23bf849e2b6b271ac51b50e46f124dd4
JaimeSerrano15 <00025318@uca.edu.sv>
2019-11-19 10:45:06 -0600

    Probando branch

--- yugi-backend/routes/cards.js
+++ yugi-backend/routes/cards.js
@@ -8,6 +8,7 @@ router.get('/', function(req,res,next){
 
 router.post('/', function(req,res,next){
     cardController.save(req,res,next);
+    console.log("Hay que crear cartas");
 })
 
 module.exports = router;
