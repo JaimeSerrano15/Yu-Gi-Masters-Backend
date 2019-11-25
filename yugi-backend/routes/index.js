758a40009aadb749b716e2bd3c803e7b485754f7
JaimeSerrano15 <00025318@uca.edu.sv>
2019-11-25 15:46:53 -0600

    Agregando vistas pug

--- yugi-backend/routes/index.js
+++ yugi-backend/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('register', { title: 'Express' });
 });
+
+router.get('/info', function(req,res,next){
+  res.render('info');
+})
 
 module.exports = router;
