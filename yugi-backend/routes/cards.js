d641638811cb3e7ac3e080b36a987f5f371545dd
AlejandroPinto99 <00156618@uca.edu.sv>
2019-11-25 14:23:58 -0600

    Trasladando elementos y ventanas de React a PUG

--- yugi-backend/routes/cards.js
+++ yugi-backend/routes/cards.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 var cardController = require('../controllers/CardController');
 
-router.get('/', function(req,res,next){
+/*router.get('/', function(req,res,next){
     cardController.getAll(req, res, next);
-});
+});*/
 
 router.get('/:name', cardController.getOne);
 
@@ -12,6 +12,10 @@ router.post('/', function(req,res,next){
     cardController.save(req,res,next);
     console.log("Hay que crear cartas");
     console.log("Prueba");
+})
+
+router.get('/', function(req,res,nect){
+    res.render('welcome');
 })
 
 router.delete('/:name', cardController.delete);
