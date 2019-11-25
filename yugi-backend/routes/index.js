5c3907128fc40f1c2f5f4104e3a5b66a51ae5502
AlejandroPinto99 <00156618@uca.edu.sv>
2019-11-25 15:31:39 -0600

    Paso de ventanas de ver post y foto, crear foro, y comentari de React a PUG

--- yugi-backend/routes/index.js
+++ yugi-backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('PostCreation');
+  res.render('Show_Post');
 });
 
 router.get('/prueba', function(req,res,next){
