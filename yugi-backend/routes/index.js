f61b8b3882cf6809190de1789c05deda314458ed
JaimeSerrano15 <00025318@uca.edu.sv>
2019-11-25 20:49:01 -0600

    Fix: Register and Login

--- yugi-backend/routes/index.js
+++ yugi-backend/routes/index.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('register', { title: 'Express' });
+  res.render('register', { in: false});
 });
+
+router.post('/', function(req,res,next){
+  var newUser = {
+    name: req.body.name,
+    password: req.body.pass,
+    email: req.body.mail
+  }
+
+  axios.post('http://localhost:3001/users', newUser).then((ros)=>{
+    res.redirect('/login');
+  })
+
+})
 
 router.get('/info', function(req,res,next){
   res.render('info');
@@ -12,6 +26,22 @@ router.get('/info', function(req,res,next){
 
 router.get('/login', function(req,res,next){
   res.render('login');
+})
+
+router.post('/login', function(req,res,next){
+  var confirm = {
+    name: req.body.name,
+    password: req.body.pass
+  }
+
+  axios.post('http://localhost:3001/users/log', confirm).then((ros)=>{
+    if(ros.data.auth){
+      res.redirect('/');
+    }
+    else{
+      res.redirect('/login');
+    }
+  })
 })
 
 module.exports = router;
